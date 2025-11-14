68d5445c333015f6a3084e695b3ae3425e6fc4ca
Alin1013 <2253736501@qq.com>
2025-11-14 15:14:29 +0800

    yaml文件读取

---
+++ Utils/read_yaml.py
@@ -0,0 +1,27 @@
+#yaml文件的读取
+import yaml
+from Config.Config import Config
+
+class ReadYaml(object):
+    def __init__(self,filename):
+        self.filename = filename
+
+    #定义yaml读取方法
+    def read(self):
+        with open(self.filename,mode='r',encoding='utf-8')as file:
+            data=file.read()
+
+        data_yaml=yaml.load(data,Loader=yaml.FullLoader)
+        for value in data_yaml:
+            #从用例里面的url字段，拼接url地址（用例存储在yaml文件里面）
+            if value.get("url地址") is not None:
+                value["url地址"]=Config.url+value["url地址"]
+
+            #如果有文件上传，则拼接文件上传的地址
+            if value.get("file地址") is not None:
+                value["file地址"]=Config.test_file_fir+value["file地址"]
+
+        return data_yaml
+
+if __name__=="__main__":
+    pass
