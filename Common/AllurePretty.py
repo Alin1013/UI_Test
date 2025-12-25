4f41b271322ee7e9ec135f2ac9f0b45feb16943b
Alin1013 <2253736501@qq.com>
2025-12-25 11:44:46 +0800

    Allure报告页面美化

---
+++ Common/AllurePretty.py
@@ -0,0 +1,36 @@
+#allure页面美化
+import os.path, allure, pytest, functools
+from Config.Config import Config
+
+class AllurePretty(object):
+    @classmethod
+    def AllurePretty(cls,page,CaseData):
+        allure.dynamic.feature(CaseData.get("模块"))
+        allure.dynamic.story(CaseData.get("功能"))
+        allure.dynamic.severity(CaseData.get("优先级"))
+        allure.dynamic.title(f'{CaseData.get("用例编号")}_{CaseData.get("用例标题")}')
+        if CaseData.get("是否执行") != "Y":
+            allure.dynamic.description("用例指定跳过")
+            pytest.skip("用例指定跳过")
+
+    @classmethod
+    def AllurePretty_Screenshot(cls,page,CaseData):
+        filename=os.path.join(Config.test_screenshots_dir,f"{CaseData.get('用例标题')}.png")
+        page.screenshot(path=filename)
+        allure.attach.file(source=filename,name=CaseData.get('用例标题'),attachment_type=allure.attachment_type.PNG)
+
+    @classmethod
+    def AllurePretty_Warpper(cls,func):
+        @functools.wraps(func)
+        def inner(*args,**kwargs):
+            #获取casedata
+            cls.AllurePretty(page=kwargs.get("page"),CaseData=kwargs.get("CaseData"))
+            #运行测试用例
+            r=func(*args,**kwargs)
+            #添加截图
+            cls.AllurePretty_Screenshot(page=kwargs.get("page"),CaseData=kwargs.get("CaseData"))
+            return r
+        return inner
+
+if __name__=='__main__':
+    pass
